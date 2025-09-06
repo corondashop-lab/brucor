c264bb564fcd80c93f4825346cd98571dbb860d0
corondashop-lab <corondashop@gmail.com>
2025-09-06 00:12:10 -0300

    Arreglar tailwind config y globals.css para build producción

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,css}"  // escanea todos tus archivos JS/TS/TSX y CSS dentro de src
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
