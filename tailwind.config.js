bfcaffa2c82276798c658fc3e508f78ae372fa99
corondashop-lab <corondashop@gmail.com>
2025-09-06 22:31:35 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,css}",
   ],
   theme: {
     extend: {
       colors: {
-        foreground: "#111827",
-        border: "#D1D5DB",
+        background: "#f0f0f0",  // ejemplo
+        foreground: "#111111",
+        border: "#e5e7eb",
       },
     },
   },
